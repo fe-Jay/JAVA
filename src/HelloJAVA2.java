eb817f4ebdab753a36c25aa8930ce0719ced376a
Jay <sinaelj@gmail.com>
2024-11-11 21:50:21 +0900

    섹션 1. 강의 소개와 자료 3개 ∙ (4분) 섹션 2. Hello World 5개 ∙ (46분)

---
+++ src/HelloJAVA2.java
@@ -0,0 +1,7 @@
+public class HelloJAVA2 {
+    public static void main(String[] args) {
+        System.out.println("Hello JAVA 01");
+        System.out.println("Hello JAVA 02");
+        System.out.println("Hello JAVA 03");
+    }
+}
