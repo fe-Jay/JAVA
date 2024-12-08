9874d2be7e78eae432a73fc72d6ce36b4a503381
Jay <sinaelj@gmail.com>
2024-12-08 22:57:37 +0900

    나도 할 수 있는 Java&Spring 웹 개발 종합반_30회차 // ch04

---
+++ src/BinaryExam.java
@@ -0,0 +1,14 @@
+public class BinaryExam {
+    public static void main(String[] args) {
+        // 123을 10진수, 2진수,8진수,16진수로 출력하시오
+        int data = 123;
+        String binary = Integer.toBinaryString(data);
+        System.out.println("binary = " + binary);
+
+        String octal = Integer.toOctalString(data);
+        System.out.println("octal = " + octal);
+
+        String hex = Integer.toHexString(data);
+        System.out.println("hex = " + hex);
+    }
+}
---
+++ src/operator/CarTest.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class CarTest {
+    public static void main(String[] args) {
+        // 자동차 정보를 저장
+        String model = "GENESIS GV70";
+        long distance = 100000;
+        int price = 70000000;
+        char type = 'A';
+        boolean auto = true;
+        int year = 2024;
+        double gasMi = 12.5;
+    }
+}
